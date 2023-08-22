cc3ad8d49dbcd9c39bc125156797fb8fad8217d2
kano1663012 <chauhankano456@gmail.com>
2023-08-22 15:10:06 +0530

    db/conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const uri = "mongodb://127.0.0.1:27017/test"
-mongoose.connect(process.env.url||uri)
+mongoose.connect(uri)
 .then(()=>{
     console.log('db conneted');
 }).catch((err)=>{
